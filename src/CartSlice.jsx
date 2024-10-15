10b4844c317c82ca1d2ab1f3154a4d9f7e7a936a
Sanumak <sanskruti.umak21@vit.edu>
2024-10-16 00:36:08 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,41 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0,
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
 
-    
+        } else {
+
+            state.items.push({ name, image, cost, quantity: 1 });
+            state.totalQuantity++;
+        }
+},
+    removeItem: (state, action) => {
+        const {name} = action.payload;
+        let itemsArray = [...state.items];
+        const itemToRemove =  state.items.find(item => item.name === name);
+
+        let index = itemsArray.indexOf(itemToRemove);
+        itemsArray.splice(index , 1);
+        state.items = itemsArray;
+
+        state.totalQuantity -= itemToRemove.quantity;
+    },
+
+    updateQuantity: (state, action) => {
+        const { name } = action.payload[0]; // First object of the array which is the plant item
+        const itemToUpdate = state.items.find(item => item.name === name);
+
+        state.totalQuantity += (action.payload[1] - itemToUpdate.quantity)
+
+        if (itemToUpdate) {
+            itemToUpdate.quantity = action.payload[1];  //2nd object of array - new quantity
+        }
     },
   },
 });

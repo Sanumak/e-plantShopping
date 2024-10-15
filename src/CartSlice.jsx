c0da946152e28293a5e67866093a636db1a6e965
Sanumak <sanskruti.umak21@vit.edu>
2024-10-16 00:45:03 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,45 +4,40 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
-    totalQuantity: 0,
   },
   reducers: {
     addItem: (state, action) => {
         const { name, image, cost } = action.payload;
         const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
 
+        if(existingItem) {
+            existingItem.quantity++;
         } else {
-
-            state.items.push({ name, image, cost, quantity: 1 });
-            state.totalQuantity++;
+            state.items.push({name, image, cost, quantity: 1});
         }
-},
+
+    },
     removeItem: (state, action) => {
-        const {name} = action.payload;
-        let itemsArray = [...state.items];
-        const itemToRemove =  state.items.find(item => item.name === name);
-
-        let index = itemsArray.indexOf(itemToRemove);
-        itemsArray.splice(index , 1);
-        state.items = itemsArray;
-
-        state.totalQuantity -= itemToRemove.quantity;
+        state.items = state.items.filter(item => item.name !== action.payload);
+        
     },
-
     updateQuantity: (state, action) => {
-        const { name } = action.payload[0]; // First object of the array which is the plant item
+        const { name, quantity } = action.payload;
+
         const itemToUpdate = state.items.find(item => item.name === name);
-
-        state.totalQuantity += (action.payload[1] - itemToUpdate.quantity)
-
+        
         if (itemToUpdate) {
-            itemToUpdate.quantity = action.payload[1];  //2nd object of array - new quantity
+            itemToUpdate.quantity = quantity;
         }
+
+        
     },
+    clearCart: (state) => {
+        state.items = [];
+    }
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem, removeItem, updateQuantity, clearCart } = CartSlice.actions;
 
 export default CartSlice.reducer;
